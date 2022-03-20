1232e0ea5077d86a4c9feff32092972b60ec623e
Amanpreet Singh <amanpreetsingh369@gmail.com>
2022-03-20 19:41:48 +0530

    Completed Stage 1

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const rules = document.getElementById("rules");
 const close = document.getElementById("close");
 const rulesModal = document.querySelector(".rules_modal");
 const mainContainer = document.querySelector(".main_container");
+const stage2 = document.querySelector(".start");
+const game = document.getElementById("game");
 
 rules.addEventListener("click", function () {
-	rulesModal.classList.remove("hide");
+	rulesModal.classList.remove("rules_hide");
 	mainContainer.classList.add("overlay");
 });
 
 close.addEventListener("click", function () {
-	rulesModal.classList.add("hide");
+	rulesModal.classList.add("rules_hide");
 	mainContainer.classList.remove("overlay");
+});
+
+game.addEventListener("click", function () {
+	stage2.classList.remove("hide");
+	game.classList.add("hide");
 });
