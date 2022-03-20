0731d5642fbecc36e3a2356e90f3d98c801b6b70
Amanpreet Singh <amanpreetsingh369@gmail.com>
2022-03-20 19:48:13 +0530

    changes in stage 1

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const rulesModal = document.querySelector(".rules_modal");
 const mainContainer = document.querySelector(".main_container");
 const stage2 = document.querySelector(".start");
 const game = document.getElementById("game");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
 rules.addEventListener("click", function () {
 	rulesModal.classList.remove("rules_hide");
@@ -15,7 +18,17 @@ close.addEventListener("click", function () {
 	mainContainer.classList.remove("overlay");
 });
 
-game.addEventListener("click", function () {
+rock.addEventListener("click", function () {
+	stage2.classList.remove("hide");
+	game.classList.add("hide");
+});
+
+paper.addEventListener("click", function () {
+	stage2.classList.remove("hide");
+	game.classList.add("hide");
+});
+
+scissors.addEventListener("click", function () {
 	stage2.classList.remove("hide");
 	game.classList.add("hide");
 });
